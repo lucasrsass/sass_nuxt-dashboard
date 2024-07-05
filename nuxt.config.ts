bfae17ba3489cbee6e8471cb1603ae1801e2c6db
Lucas Sass <lucasrsass@gmail.com>
2024-07-05 13:48:45 -0700

    Initial commit of SASS Nuxt UI Pro dashboard

---
+++ nuxt.config.ts
@@ -0,0 +1,37 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: [process.env.NUXT_UI_PRO_PATH || '@nuxt/ui-pro'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/fonts',
+    '@vueuse/nuxt'
+  ],
+  ui: {
+    icons: ['heroicons', 'simple-icons'],
+    safelistColors: ['primary', 'red', 'orange', 'green']
+  },
+  colorMode: {
+    disableTransition: true
+  },
+  routeRules: {
+    '/': { prerender: true }
+  },
+  devtools: {
+    enabled: true
+  },
+  typescript: {
+    strict: false
+  },
+  future: {
+    compatibilityVersion: 4
+  },
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
